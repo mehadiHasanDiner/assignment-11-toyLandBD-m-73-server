159378d1efe9fc24de45a6b6dbd2a5ad0b215ef7
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-12-22 06:32:15 +0600

    api for a  single toy created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -36,6 +36,13 @@ async function run() {
 
     app.get("/categories", async (req, res) => {
       const result = await toysCategoryCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/categories/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCategoryCollection.findOne(query);
       res.send(result);
     });
 
