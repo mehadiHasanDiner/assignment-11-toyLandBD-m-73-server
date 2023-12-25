ff7747bea7718965eecd17524b7d01f31939e2af
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-12-26 04:17:57 +0600

    delete a toy  backend api  done

--- index.js
+++ index.js
@@ -93,6 +93,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/updateToy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysAllCollection.deleteOne(query);
+      // console.log(result);
+      res.send(result);
+    });
+
     app.put("/updateToy/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
